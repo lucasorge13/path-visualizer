cea6ecbe4c839491b785049c8c93e5b8affa42b6
Lucas Sorge <lucasorge13@gmail.com>
2024-06-17 00:16:52 -0500

    A star

---
+++ src/algorithms/astar.js
@@ -0,0 +1,44 @@
+// Performs A* algorithm; returns all nodes in the order in which they were visited.
+export function astar(grid, startNode, finishNode) {
+  const visitedNodesInOrder = [];
+  startNode.distance = 0;
+  startNode.totalDistance = 0;
+  const unvisitedNodes = getAllNodes(grid);
+
+  while (unvisitedNodes.length > 0) {
+    sortNodesByTotalDistance(unvisitedNodes);
+    const closestNode = unvisitedNodes.shift();
+
+    if (closestNode.isWall) continue;
+
+    if (closestNode.distance === Infinity) return visitedNodesInOrder;
+
+    closestNode.isVisited = true;
+    visitedNodesInOrder.push(closestNode);
+
+    if (closestNode === finishNode) return visitedNodesInOrder;
+
+    updateUnvisitedNeighbors(closestNode, grid, finishNode);
+  }
+
+  return visitedNodesInOrder;
+}
+
+function sortNodesByTotalDistance(unvisitedNodes) {
+  unvisitedNodes.sort(
+    (nodeA, nodeB) => nodeA.totalDistance - nodeB.totalDistance
+  );
+}
+
+function updateUnvisitedNeighbors(node, grid, finishNode) {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+  for (const neighbor of unvisitedNeighbors) {
+    neighbor.distance = node.distance + 1;
+    neighbor.totalDistance = neighbor.distance + heuristic(neighbor, finishNode);
+    neighbor.previousNode = node;
+  }
+}
+
+function heuristic(nodeA, nodeB) {
+  return Math.abs(nodeA.row - nodeB.row) + Math.abs(nodeA.col - nodeB.col);
+}
